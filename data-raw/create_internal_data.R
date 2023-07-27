3cad200ecb06097374fd2fe15d9c2c91faecc921
schw4b <schw4b@gmail.com>
2023-07-27 15:51:30 +0200

    model update of cause-specific model with Fine and Gray model

--- data-raw/create_internal_data.R
+++ data-raw/create_internal_data.R
@@ -1,23 +1,25 @@
 # Save internal data for swt package
 
 # In order to run this code and save the data as internal data please load
-# the swt project. Then, open a project-specifc qmd pipeline from within the swt project.
+# the swt project. Then, open a project-specific qmd file from within the swt
+# project, run the pipeline until you generated the object for internal data.
+
 # Internal data is stored in R/sysdata.rda.
 
 library(fs)
 library(testit)
-PATH_PKG = file.path(fs::path_home(), "OneDrive - Swisstransplant", "Data", "swt")
+PATH_PKG = file.path(fs::path_home(), "OneDrive - Swisstransplant", "Projects", "swt")
 
 # 1. Load existing data from various projects
 load(file.path(PATH_PKG, "R", "sysdata.rda"))
 
 # 2. Run project-specific pipeline to overwrite internal data
 # KIDMO project:
-idat.fit.kidmo = fit.mod1.csh
+idat.fit.kidmo = fit.mod1.fg.coxph
 
 # 3. Quality checks: don't save sensitive data
 assert(is.null(idat.fit.kidmo$x))
-assert(is.null(idat.fit.kidmo$y))
+#assert(is.null(idat.fit.kidmo$y))
 assert(is.null(idat.fit.kidmo$model))
 
 # 4. Save as internal data
