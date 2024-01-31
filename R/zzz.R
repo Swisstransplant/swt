e282918d89aa3d7c688b40a793f9b571b84694aa
schw4b <schw4b@gmail.com>
2024-01-31 17:59:20 +0100

    edata update

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(...) {
-  packageStartupMessage("Copyright 2023 Swisstransplant. All rights reserved.")
+  packageStartupMessage("Copyright 2022, 2024 Swisstransplant. All rights reserved.")
 }
